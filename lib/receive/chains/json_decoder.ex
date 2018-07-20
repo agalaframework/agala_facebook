2abc0e10b5fa30705a81858f2c74deb49b8c3186
Dmitry Rubinstein <rubinsteindb@gmail.com>
2018-07-20 13:22:14 +0300

    Fix body to response

--- lib/receive/chains/json_decoder.ex
+++ lib/receive/chains/json_decoder.ex
@@ -4,6 +4,6 @@ defmodule Agala.Provider.Facebook.Chains.JsonDecoder do
   def init(args), do: args
 
   def call(%Agala.Conn{request: body} = conn, _opts) when is_binary(body) do
-    put_in(conn, [:body], Jason.decode!(body))
+    put_in(conn, [:request], Jason.decode!(body))
   end
 end
